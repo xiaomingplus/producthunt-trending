21943729c8d2969bd3f6e428f66325f5b7d7b870
returnyang <returnyang@gmail.com>
2019-07-04 05:05:23 +0800

    Updates

---
+++ precache-manifest.31e4880bb79ecd4fa4a728e7b1b38318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0146e03284a98b8e5dbe834a0b46d792",
+    "url": "/producthunt-trending/index.html"
+  },
+  {
+    "revision": "7da001b89df251f68d2a",
+    "url": "/producthunt-trending/static/css/2.5d832d7a.chunk.css"
+  },
+  {
+    "revision": "8dec40379f6adbaa82bc",
+    "url": "/producthunt-trending/static/css/main.0848d298.chunk.css"
+  },
+  {
+    "revision": "7da001b89df251f68d2a",
+    "url": "/producthunt-trending/static/js/2.8f1a1e8c.chunk.js"
+  },
+  {
+    "revision": "8dec40379f6adbaa82bc",
+    "url": "/producthunt-trending/static/js/main.9b04ce39.chunk.js"
+  },
+  {
+    "revision": "0e046a12c6fce441219b",
+    "url": "/producthunt-trending/static/js/runtime~main.b2d2e7aa.js"
+  }
+]);
